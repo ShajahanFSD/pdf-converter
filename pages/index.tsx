16a085809aa1503292a1bd9bc4f82efcc53db8ea
Shajahan <87851422+Shajahanhng@users.noreply.github.com>
2022-04-03 12:04:41 +0530

    all set

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
-      
+    <div className='bg-green-500'>
+      Type Script
     </div>
   )
 }
